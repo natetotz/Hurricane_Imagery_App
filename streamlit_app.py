5519b49a211921e19b4d941dda13a0ae323f6d71
natetotz <84467374+natetotz@users.noreply.github.com>
2021-08-09 13:01:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ def create_user_log(datetime, lat, lon, prediction_physical):
                      'storm_long' : lon,
                      'predicted_windspeed' : prediction_physical}
     user_log_df = pd.DataFrame(user_log_data, index = [request_datetime])
-    user_log_path = 'https://github.com/natetotz/hurricane-imagery-bucket/main/user_logs/' + str(request_datetime) + '.csv'
+    user_log_path = 'https://github.com/natetotz/hurricane-imagery-bucket/main/user_logs/test.csv'  # + str(request_datetime) + '.csv'
     user_log_df.to_csv(user_log_path)
 
 
