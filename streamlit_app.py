323be1ae14ff14c245422537b927b2d069cf385a
natetotz <84467374+natetotz@users.noreply.github.com>
2021-08-10 13:27:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ def create_user_log(datetime, lat, lon, prediction_physical):
     user_log_df = pd.DataFrame(user_log_data, index = [request_datetime])
     file_name = "UserLog" + str(request_datetime) + ".csv"
     user_log_df.to_csv(file_name)
-    st.write(file_name)
+    st.write(bash_script('pwd'))
     try:
       bash_string('cp /app/hurricane_imagery_app/' + file_name + ' /Users/nathantotz/Desktop/Hurricane_Imagery_Models/User_Logs/' + file_name)
     except:
